1460121e2e52d5dfedc5671d32bb79a12568b898
WosA <anna.wos@fujitsu.com>
2023-04-23 12:14:24 +0200

    stan na koniec zajęć

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -7,12 +7,19 @@
       <MeetingsPage :username="authenticatedUsername"></MeetingsPage>
     </div>
 
-    <div v-else>
+ 
+  
+  <div v-else>
      <button :class="signingUp ? 'button-outline' : '' "@click="signingUp = false"> Logowanie</button>
      <button :class="!signingUp ? 'button-outline' : '' "@click="signingUp = true"> Rejestracja</button>
 
-      <LoginForm v-if ="!siningUp" @login="(user) => logMeIn(user)"></LoginForm>
-      <LoginForm v-else @login="(user) => logMeIn(user)" button-label="Załóż konto"></LoginForm>
+       <div v-if="message" class="alert">
+        {{message}}
+       </div>
+    
+
+      <LoginForm v-if ="!signingUp" @login="(user) => logMeIn(user)"></LoginForm>
+      <LoginForm v-else @login="(user) => register(user)" button-label="Załóż konto"></LoginForm>
     </div>
   </div>
 </template>
@@ -22,29 +29,43 @@ import "milligram";
 import LoginForm from "./LoginForm";
 import UserPanel from "./UserPanel";
 import MeetingsPage from "./meetings/MeetingsPage";
+import axios from "axios"
 
 export default {
   components: {LoginForm, MeetingsPage, UserPanel},
   data() {
     return {
+      message: '',
       signingUp: false,
       authenticatedUsername: '',
     }
   },
   methods: {
     logMeIn(user) {
-      this.authenticatedUsername = user.login;
+      axios.post('/api/tokens', user)
+          .then(response => {
+            this.authenticatedUsername=user.login;
+            const token = response.data.token;
+            axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;  
+            axios.get('api/meetings')
+                then(response => console.log(response.data));
+                this.message = 'udało się zalogować';
+          })
+          .catch(response => {
+              this.message = 'nie udało sie zazalogować';
+          });
     },
     logMeOut() {
       this.authenticatedUsername = '';
+      delete axios.defaults.headers.common.Authorization;
     },
     register(user) {
-      axios.post('participants', user)
+      axios.post('/api/participants', user)
           .then(response => {
-              // udało się
+              this.message = 'udało się założyć konto';
           })
           .catch(response => {
-              // nie udało sie
+              this.message = 'nie udało sie założyć konta';
           });
     }
   }
@@ -55,5 +76,13 @@ export default {
 #app {
   max-width: 1000px;
   margin: 0 auto;
+}
+
+.alert {
+
+  background-color: red;
+  border: 1px solid black;
+  font-size: 20px;
+  padding: 5px;
 }
 </style>
